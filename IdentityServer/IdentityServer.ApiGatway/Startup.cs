6cf74d31b72f9fae33819fbb29368514c1675374
Sohail Shahzad <sohailshahzad932@gmail.com>
2021-08-11 17:07:18 +0500

    worked on identity with some dummy user

--- IdentityServer/IdentityServer.ApiGatway/Startup.cs
+++ IdentityServer/IdentityServer.ApiGatway/Startup.cs
@@ -1,11 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 namespace IdentityServer.ApiGatway
 {
@@ -18,7 +21,31 @@ namespace IdentityServer.ApiGatway
         }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            string textKey = "this is the secret key to add some default jwt token, lets see how it works";
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(textKey));
+            var tokenValidationParameter = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = "sohail",
+                ValidateAudience = true,
+                ValidAudience = "shahzad",
+                ValidateLifetime = true,
+                ClockSkew = System.TimeSpan.Zero,
+                RequireExpirationTime = true,
+            };
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer("TestKey", x=>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.TokenValidationParameters = tokenValidationParameter;
+                });
+            services.AddOcelot(Configuration);
         }
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
--- IdentityServer/identity.user_ms/Controllers/UserController.cs
+++ IdentityServer/identity.user_ms/Controllers/UserController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace identity.user_ms.Controllers
@@ -18,7 +23,54 @@ namespace identity.user_ms.Controllers
             User user = GetDummyData();
             return Ok(user);
         }
+        [HttpGet("login")]
+        [AllowAnonymous]
+        public IActionResult Get(string name,string password)
+        {
+            if(name == "cp" && password == "123")
+            {
+                var now = DateTime.UtcNow;
+                var claims = new Claim[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub,name),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub,now.ToUniversalTime().ToString(),ClaimValueTypes.Integer64),
+                };
+                var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("this is the secret key to add some default jwt token, lets see how it works"));
+                var tokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = signingKey,
+                    ValidIssuer = "sohail",
+                    ValidateAudience = true,
+                    ValidAudience = "shahzad",
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    RequireExpirationTime = true,
+                };
+                var jwt = new JwtSecurityToken(
+                    issuer: "sohail",
+                    audience: "shahzad",
+                    claims: claims,
+                    notBefore: now,
+                    expires: now.Add(TimeSpan.FromMinutes(2)),
+                    signingCredentials:new SigningCredentials(signingKey,SecurityAlgorithms.HmacSha256)
+               );
 
+                var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+                var responseJson = new
+                {
+                    access_token = encodedJwt,
+                    expires_in = (int)TimeSpan.FromMinutes(2000).TotalSeconds
+                };
+                return Ok(responseJson);
+            }
+            else
+            {
+                return BadRequest("User name or password is incorrect");
+            }
+            
+        }
         private User GetDummyData()
         {
             User user = new User
